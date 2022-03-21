406a186f0c2e75218b76549596dc4f6de018edf0
GIAMPAOLO DELGADO <giampaolo.delgadoo@ug.edu.ec>
2022-03-21 12:29:10 -0500

    Database default

--- config/database.js
+++ config/database.js
@@ -1,20 +1,11 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "ec2-54-89-49-242.compute-1.amazonaws.com"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "d5fmbunqnscd49"),
-        username: env("DATABASE_USERNAME", "wjazjtibslcvti"),
-        password: env("DATABASE_PASSWORD", "f46196931b507b473a1b3c06bd7669d3379baf67adcd06d1f9f2a8a5d180355a"),
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      options: {},
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
